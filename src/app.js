d18a24cec13166afb796ff5b5610d76a5b432a66
Chukwuma Obasi <ccobasi8@gmail.com>
2021-06-17 21:03:45 +0100

    Add container for humidity and pressure

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ const weatherReport = (mainTemp, cityName, countryName, lowTemp, highTemp,
     dataContainer.setAttribute('class', 'd-flex justify-content-center color-font');
     cardContainer.appendChild(dataContainer);
 
-    // max temperature conatainer
+    // max temperature container
   const maxTempContainer = document.createElement('div');
 
   const maxTemp = document.createElement('h4');
@@ -37,7 +37,7 @@ const weatherReport = (mainTemp, cityName, countryName, lowTemp, highTemp,
 
   dataContainer.appendChild(maxTempContainer);
 
-  // min temperature conatainer
+  // min temperature container
   const minTempContainer = document.createElement('div');
 
   const minTemp = document.createElement('h4');
@@ -55,3 +55,39 @@ const weatherReport = (mainTemp, cityName, countryName, lowTemp, highTemp,
   minTempContainer.appendChild(minTempValue);
 
   dataContainer.appendChild(minTempContainer);
+
+  // humidity container
+  const humidityContainer = document.createElement('div');
+
+  const minHumidity = document.createElement('h4');
+  minHumidity.setAttribute('class', 'px-3 fw-bold');
+  const minHumidityText = document.createTextNode('Humidity');
+  minHumidity.appendChild(minHumidityText);
+
+  const minHumidityValue = document.createElement('h4');
+  minHumidityValue.setAttribute('class', 'px-5 fw-bold');
+  const humidityValue = document.createTextNode(`${humidity}%`);
+  minHumidityValue.appendChild(humidityValue);
+
+  humidityContainer.appendChild(minHumidity);
+  humidityContainer.appendChild(minHumidityValue);
+
+  dataContainer.appendChild(humidityContainer);
+
+  // pressure container
+  const pressureContainer = document.createElement('div');
+
+  const minPressure = document.createElement('h4');
+  minPressure.setAttribute('class', 'px-3 fw-bold');
+  const minPressureText = document.createTextNode('Pressure');
+  minPressure.appendChild(minPressureText);
+
+  const minPressureValue = document.createElement('h4');
+  minPressureValue.setAttribute('class', 'px-5 fw-bold');
+  const pressureValue = document.createTextNode(`${pressure}ha`);
+  minPressureValue.appendChild(pressureValue);
+
+  pressureContainer.appendChild(minPressure);
+  pressureContainer.appendChild(minPressureValue);
+
+  dataContainer.appendChild(pressureContainer);
